ee2879e7893b20ae4aaee1023441d4d8ec1d9b98
JavRomero89 <javieromero23@gmail.com>
2023-07-05 18:32:31 +0200

    Advance

---
+++ main.js
@@ -0,0 +1,9 @@
+let habilidades = {
+    'health': 3,
+    'boost': 2,
+    'leech': 50,
+    'fireBall': 24,
+    'ironBody': 8
+}
+let nameClassMate =["Morales", "Otniel", "Oscar", "Santiago", "Javier"]
+let nameTeacher = ["Ángel","Marcos","Pedro","Daniel","Millard"]
